c5168783341284592bbb7abc3a4d982e38f952c9
rohildshah <shah5963@gmail.com>
2020-09-04 19:55:02 -0700

    Spruce up design

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const svars = ["sa", "re", "ga", "ma", "pa", "dha", "ni"]
-const max = 0;
-const min = 7;
+const swars = ["sa", "re", "ga", "ma", "pa", "dha", "ni", "ma\u030D", "re", "ga", "dha", "ni"]
 
 function buttonClick() {
-    document.getElementById('first-svar').innerHTML = svars[Math.floor(Math.random() * (max - min) + min)];
-    document.getElementById('second-svar').innerHTML = svars[Math.floor(Math.random() * (max - min) + min)];
-    // console.log(svars[Math.floor(Math.random() * (max - min) + min)]);
+    var [firstSwar, secondSwar] = randomUnique(0, 12);
+
+    swarString = firstSwar > 7 ? "<u>" + swars[firstSwar] + "</u>" : swars[firstSwar];
+    document.getElementById('first-swar').innerHTML = swarString;
+
+    swarString = secondSwar > 7 ? "<u>" + swars[secondSwar] + "</u>" : swars[secondSwar];
+    document.getElementById('second-swar').innerHTML = swarString;
 }
+
+function randomUnique(min, max) {
+    var first = Math.floor(Math.random() * (max - min) + min);
+    var second = Math.floor(Math.random() * (max - min) + min);
+    while (second == first) {
+        second = Math.floor(Math.random() * (max - min) + min);
+    }
+
+    return [first, second];
+}
+
+buttonClick();
