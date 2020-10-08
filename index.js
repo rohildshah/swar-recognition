796aceb6a876beb7e83d7d1ca00c475f8cac577f
rohildshah <shah5963@gmail.com>
2020-10-07 17:45:05 -0700

    Almost working; few bugs present

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
-const swars = ["sa", "re", "ga", "ma", "pa", "dha", "ni", "ma\u030D", "re", "ga", "dha", "ni"]
+const swars = [//lower
+               "pa\u0323",
+               "d\u0332h\u0332\u0323a\u0332",
+               "dh\u0323a",
+               "n\u0332i\u0332\u0323",
+               "ni\u0323",
+               //middle
+               "sa",
+               "r\u0332e\u0332",
+               "re",
+               "ga\u0332",
+               "ga",
+               "ma",
+               "ma\u030D",
+               "pa",
+               "d\u0332h\u0332a\u0332",
+               "dha",
+               "n\u0332i\u0332",
+               "ni",
+               //upper
+               "sa\u0307",
+               "r\u0332e\u0332\u0307",
+               "re\u0307",
+               "ga\u0332\u0307",
+               "ga\u0307",
+               "ma\u0307",
+               "m\u0307a\u030D",
+               "pa\u0307"]
 
-function buttonClick() {
-    var [firstSwar, secondSwar] = randomUnique(0, 12);
+function screenClick() {
+    numSwars = document.getElementsByClassName('active')[0].children[0].id;
+    var randomSwars = randomUnique(0, 25, numSwars);
 
-    swarString = firstSwar > 7 ? "<u>" + swars[firstSwar] + "</u>" : swars[firstSwar];
-    document.getElementById('first-swar').innerHTML = swarString;
-
-    swarString = secondSwar > 7 ? "<u>" + swars[secondSwar] + "</u>" : swars[secondSwar];
-    document.getElementById('second-swar').innerHTML = swarString;
-}
-
-function randomUnique(min, max) {
-    var first = Math.floor(Math.random() * (max - min) + min);
-    var second = Math.floor(Math.random() * (max - min) + min);
-    while (second == first) {
-        second = Math.floor(Math.random() * (max - min) + min);
+    for (let i = 0; i < 4; i++) {
+        if (i < randomSwars.length) {
+            document.getElementById("" + i + "-swar").style.display = "block";
+            document.getElementById("" + i + "-swar").innerHTML = swars[randomSwars[i]];
+        } else {
+            document.getElementById("" + i + "-swar").style.display = "none";
+            // console.log(document.getElementById("" + i + "-swar"))
+        }
     }
-
-    return [first, second];
+    
 }
 
-buttonClick();
+function randomUnique(min, max, number) {
+    let randoms = [];
+    let random;
+    while (number--) {
+        random = Math.floor(Math.random() * (max - min) + min);
+        if (randoms.length != 0) {
+            while (randoms[randoms.length - 1] == random) {
+                random = Math.floor(Math.random() * (max - min) + min);
+            }
+        }
+        randoms.push(random);
+    }
+    return randoms;
+}
+
+screenClick();
